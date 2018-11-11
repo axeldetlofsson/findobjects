a0bd3adcab6a19c2cb9e1cc6d9c54f914df0ef6b
Axel Detlofsson <axeldetlof@gmail.com>
2018-11-11 13:34:51 +0100

    added all

---
+++ FindObject.py
@@ -0,0 +1,31 @@
+import cv2
+#from cv2 import cv
+
+#method = CV_TM_SQDIFF_NORMED
+methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+# Read the images from the file
+small_image = cv2.imread('small.png')
+large_image = cv2.imread('Lolprint.png')
+
+result = cv2.matchTemplate(small_image, large_image, cv2.TM_SQDIFF_NORMED)
+
+# We want the minimum squared difference
+mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+
+# Draw the rectangle:
+# Extract the coordinates of our best match
+MPx,MPy = mnLoc
+
+# Step 2: Get the size of the template. This is the same size as the match.
+trows,tcols = small_image.shape[:2]
+
+# Step 3: Draw the rectangle on large_image
+cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
+
+# Display the original image with the rectangle around the match.
+cv2.imshow('output',large_image)
+
+# The image is only displayed if we call this
+cv2.waitKey(0)
